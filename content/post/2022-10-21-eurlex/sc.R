98412ed9c8d39271e3cb24e0513f7855e40505d1
Jacek Pardyak <jacek.pardyak@nl.team.blue>
2022-10-21 16:56:55 +0200

    eurlex

---
+++ content/post/2022-10-21-eurlex/sc.R
@@ -0,0 +1,17 @@
+library('eurlex')
+library(dplyr)
+content <- elx_make_query(resource_type = "directive", 
+                          directory = "16", # 16 Science, information, education and culture
+                          include_citations = T,  # references (links) to other legal documents
+                          include_force = T) %>% # boolean in_force
+  elx_run_query() %>% filter(force == "true") %>% 
+  select(celex, citationcelex)
+content
+
+elx_fetch_data(
+  url = "31977L0486",
+  type = c("ids"))
+
+elx_fetch_data(
+  url = "http://publications.europa.eu/resource/celex/32000L0031", 
+  type = c("ids"))
