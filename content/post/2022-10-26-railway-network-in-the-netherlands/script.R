6b366c0f7b57e32ea58206f2f894ab1734e41f4c
Jacek Pardyak <jacek.pardyak@nl.team.blue>
2022-11-03 16:40:53 +0100

    phase 1

--- content/post/2022-10-26-railway-network-in-the-netherlands/script.R
+++ content/post/2022-10-26-railway-network-in-the-netherlands/script.R
@@ -13,7 +13,29 @@ df <- "https://en.wikipedia.org/wiki/Railway_stations_in_the_Netherlands" %>%
 #  write_csv("./content/post/2022-10-26-railway-network-in-the-netherlands/network.csv")
 
 # make visNetwork
-read_csv("./content/post/2022-10-26-railway-network-in-the-netherlands/network.csv") %>%
-  graph_from_data_frame() %>%
+graph <- read_csv("./content/post/2022-10-26-railway-network-in-the-netherlands/network.csv",
+         comment = "#") %>%
+  graph_from_data_frame()
+graph %>%
   toVisNetworkData() -> df
-visNetwork(nodes = df$nodes, edges = df$edges) 
+visNetwork(nodes = (df$nodes %>% 
+                      arrange(id) ),
+           edges = df$edges) %>%
+  visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE)
+
+
+
+library('networkD3')
+
+members <- rep(1, length(graph))
+# Convert to object suitable for networkD3
+df_d3 <- igraph_to_networkD3(graph, group = members)
+
+# Create force directed network plot
+forceNetwork(Links = df_d3$links, Nodes = df_d3$nodes, 
+             Source = 'source', Target = 'target', 
+             NodeID = 'name', Group = 'group', fontSize = 12)
+
+radialNetwork(List = df_d3, fontSize = 10, opacity = 0.9)
+
+?forceNetwork
