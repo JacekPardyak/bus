dcb4d65a0d8c7b2c106a5fbb55f10d58f9251403
unknown <jacek.pardyak@gmail.com>
2022-11-27 12:34:35 +0100

    ccc

--- content/post/2022-10-21-eurlex/sc.R
+++ content/post/2022-10-21-eurlex/sc.R
@@ -1,11 +1,11 @@
 library('eurlex')
 library(dplyr)
-content <- elx_make_query(resource_type = "directive", 
+content <- elx_make_query(resource_type = "any", 
                           directory = "16", # 16 Science, information, education and culture
                           include_citations = T,  # references (links) to other legal documents
                           include_force = T) %>% # boolean in_force
   elx_run_query() %>% filter(force == "true") %>% 
-  select(celex, citationcelex)
+  select(celex, citationcelex) 
 content
 
 elx_fetch_data(
@@ -15,3 +15,5 @@ elx_fetch_data(
 elx_fetch_data(
   url = "http://publications.europa.eu/resource/celex/32000L0031", 
   type = c("ids"))
+
+?elx_make_query
