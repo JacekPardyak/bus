71af2151474ce6fcaaa243edd27c4806bc1b9b99
JacekPardyak <jacek.pardyak@gmail.com>
2026-02-18 13:03:47 +0100

    nbg

--- gvb/_script.R
+++ gvb/_script.R
@@ -1,4 +1,4 @@
-DiagrammeR::grViz("
+g <- "
 graph G {
   layout=neato
   splines=true
@@ -7,5 +7,22 @@ graph G {
   B [pos=\"2,0!\"]
   A -- B [pos=\"e,2,0 1,1 0,0\"]
 }
-") %>%
-  DiagrammeRsvg::export_svg() %>% writeLines("test.svg")
+"
+g |> writeLines("test.gv")
+
+g |> DiagrammeR::grViz() |>
+  DiagrammeRsvg::export_svg() |>
+  writeLines("test.svg")
+
+ig <- read_graph("test.gv")
+
+# 4. Export to GraphML
+write_graph(ig, file = "my_graph.graphml", format = "graphml")
+
+
+?DiagrammeR
+
+library(igraph)
+
+ig <- to_igraph(NRD)
+write_graph(ig, "NRD.graphml", format = "graphml")
